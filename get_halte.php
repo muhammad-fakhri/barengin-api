ec7efe6c18e8b56713227efac2764f2011680f4d
muhammad-fakhri <muhammad_fakhri@apps.ipb.ac.id>
2019-05-08 10:16:14 +0700

    Add get halte funtion

--- get_halte.php
+++ get_halte.php
@@ -4,16 +4,24 @@ include 'db_connect.php';
 
 $query = mysqli_query($connect, "SELECT * FROM halte_lot");
 if (mysqli_num_rows($query)) {
-    $row  = mysqli_fetch_assoc($query_select);
+
+    // $data_halte = array();
+    // while($result =mysqli_fetch_assoc($query)){
+    //     $data_halte[]=$result;
+    // }
+
+    $row  = mysqli_fetch_assoc($query);
     $data = array(
         'message' => "Data halte nya nih !",
+        // 'data'    => $data_halte,
         'data'    => $row,
-        'status'  => "200"
+        'status'  => "200",
     );
 } else {
     $data = array(
         'message' => "Gak ada datanya halte :( !",
-        'status'  => "404"
+        'status'  => "404",
     );
 }
 echo json_encode($data);
+?>
