d17a4c3bcb0e15ffdc4d287e2d4a291e1a99ac4c
muhammad-fakhri <muhammad_fakhri@apps.ipb.ac.id>
2019-05-08 13:42:03 +0700

    Fix get_halte.php

--- get_halte.php
+++ get_halte.php
@@ -5,16 +5,16 @@ include 'db_connect.php';
 $query = mysqli_query($connect, "SELECT * FROM halte_lot");
 if (mysqli_num_rows($query)) {
 
-    // $data_halte = array();
-    // while($result =mysqli_fetch_assoc($query)){
-    //     $data_halte[]=$result;
-    // }
+    $data_halte = array();
+    while($result =mysqli_fetch_assoc($query)){
+        $data_halte[]=$result;
+    }
 
-    $row  = mysqli_fetch_assoc($query);
+    // $row  = mysqli_fetch_assoc($query);
     $data = array(
         'message' => "Data halte nya nih !",
-        // 'data'    => $data_halte,
-        'data'    => $row,
+        'data'    => $data_halte,
+        // 'data'    => $row,
         'status'  => "200",
     );
 } else {
